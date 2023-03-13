26ae06b7725798f01dcd8f5dfa8de5f37cb15766
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-03-13 14:15:52 +0100

    The final version

--- assets/js/minTraktor.js
+++ assets/js/minTraktor.js
@@ -10,3 +10,14 @@ window.addEventListener('load', (e)=>{
         myElement.classList.toggle('showImg');
     };
 });
+
+const btn = document.querySelectorAll('#faq-list button');
+
+
+for (let index = 0; index < btn.length; index++) {
+    btn[index].addEventListener('click', (e) => {
+        let section = e.target.parentNode.parentNode;
+        section.classList.toggle('active');
+    
+    })
+};
