774b777f7e4c1d873c62ddcb49bdbe9900dafdea
Degner3 <niklaschristiansen@live.dk>
2023-03-13 13:55:40 +0100

    Niklas

--- assets/js/section.js
+++ assets/js/section.js
@@ -7,5 +7,13 @@ for (let index = 0; index < btn.length; index++) {
     btn[index].addEventListener('click', (e) => {
         let section = e.target.parentNode.parentNode;
         section.classList.toggle('active');
+    
     })
+
+    
 }
+
+
+
+
+
